638bc6f9e35ff4ea1d2cfa035fb490b36850a01a
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-01-01 13:09:55 +0530

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,9 @@
-import { createConnection } from "mysql2";
+// import { createConnection } from "mysql2";
 
-export const db = createConnection({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_EXPOSE_PORT),
-    database: process.env.DB_NAME,
-    user: process.env.FLEXIBASE_ADMIN_USER,
-    password: process.env.FLEXIBASE_ADMIN_PASSWORD
-});
+// export const db = createConnection({
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_EXPOSE_PORT),
+//     database: process.env.DB_NAME,
+//     user: process.env.FLEXIBASE_ADMIN_USER,
+//     password: process.env.FLEXIBASE_ADMIN_PASSWORD
+// });
